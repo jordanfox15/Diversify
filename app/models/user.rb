1dbf837146a55833d7efa6e862912800bfdf1c65
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-03-10 20:03:31 -0800

    Add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :user_interests
   has_many :interests, through: :user_interests
+  has_many :first_user_matches, foreign_key: "first_user_id", class_name: "Match"
+  has_many :second_user_matches, foreign_key: "second_user_id", class_name: "Match"
 end
