8669c96d72c34c3d838d17a20f645abe781f1327
Jordan Fox <email": null>
2016-03-11 11:39:29 -0800

    Add migration to change ethnicity column to race

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311023241) do
+ActiveRecord::Schema.define(version: 20160311193327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20160311023241) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_hash"
-    t.string   "ethnicity"
+    t.string   "race"
     t.string   "email"
     t.string   "religion"
     t.string   "sex_or"
