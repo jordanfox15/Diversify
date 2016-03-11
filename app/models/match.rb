d94ab9f67f6a011846bb834189c13ea39650cb94
Jordan Fox <email": null>
2016-03-11 14:16:41 -0800

    Add more to seed file

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ActiveRecord::Base
  belongs_to :first_user, class_name: "User"
- belongs_to :second_user, class_name: "User" 
+ belongs_to :second_user, class_name: "User"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,43 @@ sex_ors = ["Heterosexual", "Gay/Lesbian", "Queer" "Bisexual", "Transgender", "As
 ses = ["Upper-SES", "Middle-SES", "Low-SES"]
 
 countries = ["US", "Mexico", "Canada", "England", "Russia", "Turkey", "Ethiopia", "France", "Germany", "Brazil", "South Africa", "India", "Iran", "Romania", "Ireland", "Scotland", "Sweden", "Chile", "Argentina", "Kuwait", "Saudi Arabia", "Jordan", "China", "Japan", "Denmark", "Cambodia", "Afghanistan", "Mongolia", "Lithuania", "Czech Republic", "Cuba", "Australia", "New Zealand"]
+
+100.times do
+  users = User.create([first_name: Faker::Name.first_name,
+                       last_name: Faker::Name.last_name,
+                       password: "password",
+                       race: race.sample,
+                       email: Faker::Internet.email,
+                       religion: religions.sample,
+                       sex_or: sex_ors.sample,
+                       ses: ses.sample,
+                       country: countries.sample,
+                       gender: ["male", "female", "other"].sample,
+                       age: rand(18..80)])
+end
+
+250.times do
+  matches = Match.create([first_user_id: rand(1..100),
+                          second_user_id: rand(1..100)])
+end
+
+Match.all.each do |match|
+  message = Message.create([text: Faker::Lorem.sentences,
+                            sender_id: match.first_user_id,
+                            recipient_id: match.second_user_id,
+                            match_id: match.id])
+
+  message = Message.create([text: Faker::Lorem.sentences,
+                            sender_id: match.second_user_id,
+                            recipient_id: match.first_user_id,
+                            match_id: match.id])
+end
+
+
+
+
+
+
+
+
+
