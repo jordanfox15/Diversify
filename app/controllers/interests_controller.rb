18e6ecd0f3045ce40b8f3b2007dfcb893521a36f
Jordan Fox <email": null>
2016-03-11 14:47:19 -0800

    Add completed message, interest controllers

---
+++ app/controllers/interests_controller.rb
@@ -0,0 +1,10 @@
+class InterestsController < ApplicationController
+
+  def index
+    @interests = Interest.all
+    respond_to do |format|
+      format.json {render json: @interests}
+    end
+  end
+
+end
