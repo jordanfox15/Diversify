c75782eae1a3827aaa04f5a03f9cad93abaf029c
Monique williamson <monique@Moniques-MacBook-Air.local>
2016-03-16 23:27:09 -0700

    can edit name on demo, cleaned up pages added buttons

--- app/controllers/api/demographics_controller.rb
+++ app/controllers/api/demographics_controller.rb
@@ -21,10 +21,15 @@ class Api::DemographicsController < ApplicationController
     user = current_user
     demographic = user.demographic
     demographic.update(demographic_params)
-    render json: demographic
+    user.update(user_params)
+    render json: [demographic, user]
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :race, :email, :religion, :sex_or, :ses, :country, :gender, :age, :avatar, :password, :password_confirmation, :interest_ids => [] )
+  end
 
   def demographic_params
     params.require(:demographic).permit(:age, :gender_id, :country_id, :race_id, :religion_id, :sex_or_id, :ses_id)
